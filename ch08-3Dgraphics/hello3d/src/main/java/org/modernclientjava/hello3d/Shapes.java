92660293b140dce37ee103fbb78d065d686d1116
Johan Vos <johan@lodgon.com>
2019-08-06 21:35:34 +0200

    process review comments

--- ch08-3Dgraphics/hello3d/src/main/java/org/modernclientjava/hello3d/Shapes.java
+++ ch08-3Dgraphics/hello3d/src/main/java/org/modernclientjava/hello3d/Shapes.java
@@ -3,6 +3,7 @@ package org.modernclientjava.hello3d;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -23,7 +24,7 @@ public class Shapes extends Application {
         Group root = new Group(sphere, box);
         root.setTranslateX(320);
         root.setTranslateY(240);
-        Scene scene = new Scene(root, 640, 480);
+        Scene scene = new Scene(root, 640, 480, true, SceneAntialiasing.BALANCED);
         stage.setTitle("JavaFX 3D Shapes");
         stage.setScene(scene);
         stage.show();
