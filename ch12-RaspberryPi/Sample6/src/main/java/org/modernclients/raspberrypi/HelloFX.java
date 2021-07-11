d060744ef474bbc25413c62ac2e1479b33f16294
José Pereda <pereda@eii.uva.es>
2021-07-11 22:09:29 +0200

    Add fullscreen support to Sample6 (#19)
    Co-authored-by: jose.pereda <jose.pereda@gluonhq.com>

--- ch12-RaspberryPi/Sample6/src/main/java/org/modernclients/raspberrypi/HelloFX.java
+++ ch12-RaspberryPi/Sample6/src/main/java/org/modernclients/raspberrypi/HelloFX.java
@@ -2,11 +2,13 @@ package org.modernclients.raspberrypi;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -26,6 +28,14 @@ public class HelloFX extends Application {
         Scene scene = new Scene(root, 640, 480);
         scene.getStylesheets().add(HelloFX.class.getResource("styles.css").toExternalForm());
         stage.setScene(scene);
+
+        if (Boolean.getBoolean("use.fullscreen")) {
+            Rectangle2D bounds = Screen.getPrimary().getBounds();
+            stage.setX(bounds.getMinX());
+            stage.setY(bounds.getMinY());
+            stage.setWidth(bounds.getWidth());
+            stage.setHeight(bounds.getHeight());
+        }
         stage.show();
     }
 
